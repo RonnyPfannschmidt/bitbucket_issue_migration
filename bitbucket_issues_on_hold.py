9c95c150443459ac54f5d168f623df80c898bc92
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2016-09-17 07:39:06 +0200

    modernize the on hold script

--- bitbucket_issues_on_hold.py
+++ bitbucket_issues_on_hold.py
@@ -1,33 +1,60 @@
 """
-Quick and dirty script to mark all issues in a bitbucket repository as "on hold" and post 
+Quick and dirty script to mark all issues
+in a bitbucket repository as "on hold" and post
 a comment about the move to GH.
 """
 
 import requests
+import click
+
+URL_FMT = "https://bitbucket.org/api/1.0/repositories/{repo}/issues/{extra}"
+MSG_FMT = ("This issue has been moved to GitHub: "
+           "https://github.com/{targetrepo}/issues/{issue_id}")
+SEP = '-' * 10
 
 
-auth = ('user', 'pass')
-max_issues = 768
-for issue_id in xrange(1, max_issues+1):
-    url = 'https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/issues/{issue_id}/comments'
-    url = url.format(accountname='pytest-dev', repo_slug='pytest', issue_id=issue_id)
-    print '-' *10, issue_id
-    data = {
-        "content": "This issue has been moved to GitHub: https://github.com/pytest-dev/pytest/issues/{issue_id}".format(issue_id=issue_id),
-    }
-    respo = requests.post(url, data=data, auth=auth)
-    if respo.status_code != 200:
-        print respo.text
+@click.command()
+@click.argument('repo')
+@click.argument('targetrepo')
+@click.argument('user')
+@click.argument('password')
+@click.option('--dry-run', is_flag=True)
+def main(repo, targetrepo, user, password, dry_run):
+    auth = user, password
 
-    #print respo.json()
+    def url(extra, **kw):
+        if kw:
+            extra = extra.format(**kw)
+        return URL_FMT.format(repo=repo, extra=extra)
 
-    url = 'https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/issues/{issue_id}'
-    url = url.format(accountname='pytest-dev', repo_slug='pytest', issue_id=issue_id)
-    data = {
-        'status': 'on hold',
-    }
-    respo = requests.put(url, data=data, auth=auth)
-    if respo.status_code != 200:
-        print respo.text
+    session = requests.Session()
+    session.auth = auth
 
+    with session:
+        meta = session.get(url('?limit=0'))
+        if meta.status_code != 200:
+            print(meta.text)
+        max_issues = meta.json()['count']
 
+    if dry_run:
+        post = print
+    else:
+        def post(part, data):
+            response = session.post(url(part), data=data)
+            if response.status_code != 200:
+                print(response.text)
+
+    for issue_id in range(1, max_issues+1):
+        print(SEP, issue_id)
+        post(
+            '{issue_id}/comments'.format(issue_id=issue_id),
+            {"content": MSG_FMT.format(
+                issue_id=issue_id,
+                targetrepo=targetrepo)}
+        )
+        post(
+            '{issue_id}'.format(issue_id=issue_id),
+            {'status': 'on hold'}
+        )
+
+main()
