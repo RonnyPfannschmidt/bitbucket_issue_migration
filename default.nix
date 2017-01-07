f17c354e1e19db221b721b9d4a2537d8733b8986
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2017-01-07 17:54:57 +0100

    fix tests

--- default.nix
+++ default.nix
@@ -4,14 +4,18 @@ in
   with pkgs.python36Packages;
   buildPythonPackage {
     name = "migrate";
+    src = ./.;
     buildInputs = [
+        pkgs.git
         pytest_30
         requests2
         attrs
         parsedatetime
         setuptools_scm
+        pathlib2
         #d2tod1
         click
+        mock
         #wait_for
         ] ;
   }
